65596e7b44b867314195366dad3036ec8c2a4192
Claudia Belforte <claubelforte@gmail.com>
2020-11-24 14:21:41 -0300

    iniciando

---
+++ js.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(listo);
+
+
+function listo()
+{
+    jQuery(".navbar-toggler").click(function(e){
+        e.preventDefault();
+        jQuery(".navbar i")togglerClass("ion-icon name="close-outline"></ion-icon");
+        
+
+    });
+
+}
